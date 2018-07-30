1e4a1dcc8d88376c574c1473a1d48396e5817be0
Brandon Banks <b93banks@gmail.com>
2018-07-30 09:32:25 -0400

    scan directory and grab keys off file

--- parity/cli.py
+++ parity/cli.py
@@ -1,2 +1,16 @@
+import os
+
+
 def main():
-    print('hello world')
+    path = './'
+    env_file_paths = []
+    for file in os.scandir(path):
+        if file.is_file():
+            if '.env' in file.path:
+                env_file_paths.append(file.path)
+    for path in env_file_paths:
+        f = open(path, 'r', encoding='utf-8')
+        keys = []
+        for line in list(f):
+            keys.append(line.split('=')[0])
+    print(keys)
