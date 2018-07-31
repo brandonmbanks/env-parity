b1884d75608674efa632a895a1b44252e176c11b
Brandon Banks <b93banks@gmail.com>
2018-07-31 16:22:50 -0400

    basic env checks

--- parity/cli.py
+++ parity/cli.py
@@ -8,9 +8,19 @@ def main():
         if file.is_file():
             if '.env' in file.path:
                 env_file_paths.append(file.path)
+
+    all_keys = []
     for path in env_file_paths:
         f = open(path, 'r', encoding='utf-8')
         keys = []
         for line in list(f):
             keys.append(line.split('=')[0])
-    print(keys)
+        if len(all_keys) == 0:
+            all_keys = keys
+        diff = list(set(keys).symmetric_difference(set(all_keys)))
+        if len(diff) > 0:
+            for key in diff:
+                if key in keys:
+                    print('extra {} key in {}'.format(key, path))
+                else:
+                    print('missing {} key in {}'.format(key, path))
